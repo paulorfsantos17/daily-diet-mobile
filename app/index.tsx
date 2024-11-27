3d2106eab9e0befdea2f7f40779e4f7186e746e8
Paulo Ricardo Santos <42043880+paulorfsantos17@users.noreply.github.com>
2024-11-27 19:30:06 -0300

    chore: setup SVG

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from 'react-native'
+import { HStack } from '@/components/ui/hstack'
+import { VStack } from '@/components/ui/vstack'
+import Logo from '@/assets/svgs/logo.svg'
+import { ArrowLeft } from 'phosphor-react-native'
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-red-dark">Home Screen</Text>
-    </View>
+    <VStack className="m-6 mt-16">
+      <HStack>
+        <Logo />
+        <ArrowLeft />
+      </HStack>
+    </VStack>
   )
 }
