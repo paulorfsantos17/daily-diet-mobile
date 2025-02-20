15483d69401f9f1a289a42a2f7c296023c1d4611
Paulo Ricardo Santos <42043880+paulorfsantos17@users.noreply.github.com>
2025-02-20 10:07:13 -0300

    feat: add alert in page Meal

--- app/meal.tsx
+++ app/meal.tsx
@@ -1,3 +1,4 @@
+import { AlertContent, AlertText } from '@/components/alert'
 import { Box } from '@/components/ui/box'
 import { Button, ButtonText } from '@/components/ui/button'
 import { Heading } from '@/components/ui/heading'
@@ -11,43 +12,58 @@ import { View } from 'react-native'
 
 export default function Meal() {
   return (
-    <VStack className="flex-1 bg-green-light gap-8">
-      <HStack className="mt-20 mx-6">
-        <Link href="/">
-          <ArrowLeft size={24} color={colors.gray[800]} />
-        </Link>
-        <Heading className="text-lg  flex-1 text-center">Refeição</Heading>
-      </HStack>
-
-      <VStack className="flex-1 gap-8 bg-gray-100 rounded-t-[20px] px-6 pt-8 pb-8">
-        <Box className="gap-2">
-          <Heading>Sanduíche</Heading>
-          <Text className="text-gray-800">
-            Sanduíche de pão integral com atum e salada de alface e tomate
-          </Text>
+    <>
+      <AlertContent>
+        <AlertText message="Deseja realmente excluir o registro da refeição?" />
+        <Box className="flex-row   gap-4 justify-center">
+          <Button variant="outline" className="w-1/2">
+            <ButtonText>Cancelar</ButtonText>
+          </Button>
+          <Button className="w-1/2">
+            <ButtonText>Sim, excluir</ButtonText>
+          </Button>
         </Box>
-
-        <Box className="gap-2">
-          <Heading size="xs">Data e hora</Heading>
-
-          <Text className="text-gray-800">20/08/2023 ás 20:00</Text>
-        </Box>
-        <HStack className="items-center px-4 py-2 gap-2  bg-gray-200 rounded-full max-w-[40%]">
-          <View className="h-2 w-2 bg-green-dark rounded-full" />
-          <Text size="sm" className="text-gray-900 ">
-            Dentro da dieta
-          </Text>
+      </AlertContent>
+      <VStack className="flex-1 bg-green-light gap-8">
+        <HStack className="mt-20 mx-6">
+          <Link href="/">
+            <ArrowLeft size={24} color={colors.gray[800]} />
+          </Link>
+          <Heading className="text-lg  flex-1 text-center">Refeição</Heading>
         </HStack>
 
-        <VStack className="flex-1 justify-end gap-2">
-          <Button>
-            <ButtonText>Editar refeição</ButtonText>
-          </Button>
-          <Button action="secondary">
-            <ButtonText>Excluir refeição</ButtonText>
-          </Button>
+        <VStack className="flex-1 gap-8 bg-gray-100 rounded-t-[20px] px-6 pt-8 pb-8">
+          <Box className="gap-2">
+            <Heading>Sanduíche</Heading>
+            <Text className="text-gray-800">
+              Sanduíche de pão integral com atum e salada de alface e tomate
+            </Text>
+          </Box>
+
+          <Box className="gap-2">
+            <Heading size="xs">Data e hora</Heading>
+
+            <Text className="text-gray-800">20/08/2023 ás 20:00</Text>
+          </Box>
+          <HStack className="items-center px-4 py-2 gap-2  bg-gray-200 rounded-full max-w-[40%]">
+            <View className="h-2 w-2 bg-green-dark rounded-full" />
+            <Text size="sm" className="text-gray-900 ">
+              Dentro da dieta
+            </Text>
+          </HStack>
+
+          <VStack className="flex-1 justify-end gap-2">
+            <Link href="/edit-meal" asChild>
+              <Button>
+                <ButtonText>Editar refeição</ButtonText>
+              </Button>
+            </Link>
+            <Button action="secondary">
+              <ButtonText>Excluir refeição</ButtonText>
+            </Button>
+          </VStack>
         </VStack>
       </VStack>
-    </VStack>
+    </>
   )
 }
